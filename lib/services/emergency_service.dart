76817e2828807c335938937ac5d29e7aadb83d16
Dinith Perera <dinithshenuka@gmail.com>
2025-01-27 12:26:32 +0530

    emergency service

---
+++ lib/services/emergency_service.dart
@@ -0,0 +1,7 @@
+// FILE: emergency_service.dart
+// DESCRIPTION: This file contains the implementation of the emergency button functionalities. 
+// AUTHOR: Dinith Perera
+// DATE: 27/01/2025
+// LAST MODIFIED: 27/01/2025 | Dinith | Added the class EmergencyService
+
+
