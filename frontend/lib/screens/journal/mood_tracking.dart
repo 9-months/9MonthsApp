7b7ac2bdec5c12eacc8198a61385bade53418584
Dinith <dinithshenuka@gmail.com>
2025-02-11 04:21:34 +0530

    CCS-48 Mood tracking frontend

---
+++ frontend/lib/screens/journal/mood_tracking.dart
@@ -0,0 +1,9 @@
+/*
+ File: mood_tracking.dart
+ Purpose: moodtracking dashboard in the journal page
+ Created Date: CCS-48 11-02-2025
+ Author: Dinith Perera
+
+ last modified: 11-02-2025 | Dinith | CCS-49 Mood tracking 
+*/
+
---
+++ frontend/lib/services/mood_tracking_service.dart
@@ -0,0 +1,9 @@
+/*
+ File: mood_tracking_service.dart
+ Purpose: Service to handle API calls for mood tracking
+ Created Date: CCS-48 11-02-2025
+ Author: Dinith Perera
+
+ last modified: 11-02-2025 | Dinith | CCS-49 Mood tracking 
+*/
+
