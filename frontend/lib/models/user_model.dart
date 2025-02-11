26e4b48fb30f8fe3b0c88f346611023678d9817a
melzjoanne <melzjoanne@gmail.com>
2025-02-11 16:05:05 +0530

    CCS-42 User model added
    User model added to the frontend

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -6,3 +6,40 @@
 
  last modified: 
 */
+// lib/models/user_model.dart
+
+class User {
+  final String uid;
+  final String email;
+  final String username;
+  final String? location;
+  final String? phone;
+
+  User({
+    required this.uid,
+    required this.email,
+    required this.username,
+    this.location,
+    this.phone,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      uid: json['uid'],
+      email: json['email'],
+      username: json['username'],
+      location: json['location'],
+      phone: json['phone'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'email': email,
+      'username': username,
+      'location': location,
+      'phone': phone,
+    };
+  }
+}
